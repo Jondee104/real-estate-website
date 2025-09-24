6b9b1f2ab3b6ff0d7e23b8633507f9c05c29a712
Jondee104 <161108341+Jondee104@users.noreply.github.com>
2025-09-24 08:51:21 +0100

    Add files via upload

---
+++ realinteractivity.js
@@ -0,0 +1,11 @@
+function open_container(params) {
+  const open_menu = document.getElementById("open-menu-container");
+  open_menu.style.display = "block";
+}
+
+function close_container(params) {
+  const close_menu = document.getElementById("open-menu-container");
+  close_menu.style.display = "none";
+}
+
+
